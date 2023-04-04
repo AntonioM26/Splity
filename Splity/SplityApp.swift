b53ca423602373f835efd0f710e0c2d1c7b29a9d
António Almeida <antonio95@icloud.com>
2023-04-04 12:34:27 +0100

    Initial Commit

---
+++ Splity/SplityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplityApp.swift
+//  Splity
+//
+//  Created by António Almeida on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SplityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
